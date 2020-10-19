c9f0b80f769e22ae323542f10259f058d9fe01d4
Daniel Schick <daniel.schick@informatik.hs-fulda.de>
2020-10-20 00:36:26 +0200

    initial commit - WIP but works for the most

---
+++ index.js
@@ -0,0 +1,143 @@
+const {CasparCG} = require('casparcg-connection');
+const fs = require('fs');
+const http = require("http");
+const url = require("url");
+
+const address = "127.0.0.1";
+const port = "3000";
+
+const server = http.createServer(((req, res) => {
+
+    if (req.url.includes('style.css')) {
+        fs.readFile('./public/style.css', (err, css) => {
+            console.log("css requested");
+            if (err) {
+                throw err;
+            }
+            res.statusCode = 200;
+            res.setHeader("Content-type", "text/css");
+            res.write(css);
+            res.end();
+        });
+    } else if (req.url.includes('main.js')) {
+        fs.readFile('./public/main.js', (err, js) => {
+            console.log("JS requested");
+            if (err) {
+                throw err;
+            }
+            res.statusCode = 200;
+            res.setHeader("Content-type", "application/javascript");
+            res.write(js);
+            res.end();
+        });
+    } else if (req.headers.accept.includes("text/html")) {
+        fs.readFile('./public/index.html', (err, html) => {
+            console.log("html requested");
+            if (err) {
+                throw err;
+            }
+            res.statusCode = 200;
+            res.setHeader("Content-type", "text/html");
+            res.write(html);
+            res.end();
+        });
+    } else {
+        let query = url.parse(req.url, true).query;
+        if (query.line1 !== undefined && query.line2 !== undefined) {
+            console.log("subtitle requested");
+            res.statusCode = 200;
+            res.setHeader("Content-type", "text/html");
+            let lineSet = {
+                "line1": query.line1,
+                "line2": query.line2
+            }
+            con.cgUpdate(1,20,1, lineSet);
+            res.write("done!")
+            res.end();
+        }
+        if (query.songNumber!== undefined) {
+            console.log("song requested");
+            res.statusCode = 200;
+            res.setHeader("Content-type", "text/html");
+            res.write(JSON.stringify(createLinesetsFromSong(query.songNumber)));
+            res.end();
+        } else {
+            console.log("bad Request detected");
+            res.statusCode = 400;
+            res.end();
+        }
+    }
+}));
+
+server.listen(port, address, () => {
+    console.log("Server started at " + address + ":" + port);
+});
+
+let rawdata = fs.readFileSync('WDH.json');
+let songs = JSON.parse(rawdata);
+let con = new CasparCG();
+// host = 127.0.0.1, port = 5250, autoConnect = true ...
+
+let createLinesetsFromSong = function (songNumber){
+
+    let song = songs[songNumber-1];
+    let lineSets = [];
+    let emptySet = {
+        "line1": "",
+        "line2": ""
+    }
+
+    if(songNumber < 1 || songNumber > songs.length || isNaN(songNumber)){
+        return lineSets.push(emptySet);
+    }
+
+    let refrain = null;
+
+    for(let i = 0; i < song.verses.length; i++){
+        let vers = song.verses[i];
+        lineSets.push(emptySet);
+        let lines = vers.lines.split('\n');
+        for(let j = 0; j < lines.length; j+= 2){
+            if (lines[j+1] === undefined){
+                lineSets.push({
+                    "line1": "",
+                    "line2": lines[j]
+               })
+            } else {
+                lineSets.push({
+                    "line1": lines[j],
+                    "line2": lines[j+1]
+               })    
+            }
+        }
+        if(isNaN(vers.versNumber)){
+            refrain = vers;
+        }
+        else if(refrain !== null){
+            if (song.verses[i+1] === undefined && refrain !== null){
+                song.verses.splice(i+1, 0, refrain);
+            }
+            else if(!(isNaN(song.verses[i+1].versNumber) && refrain !== null)){
+                song.verses.splice(i+1, 0, refrain);
+            } 
+        }
+    }
+    lineSets.push(emptySet);
+    return lineSets;
+}
+
+lineSetTest = {
+    "line1": "das ist Timo",
+    "line2": "er fragt sich wahrschienlich noch heute, wann denn der Witz kommt"
+}
+
+lineSet = {
+    "line1": songs[364].verses[0].lines.split('\n')[0],
+    "line2": songs[364].verses[0].lines.split('\n')[1],
+}
+con.cgAdd(1,20,1,'subtitle-template', true, lineSetTest);
+
+//con.disconnect();
+
+//console.log(createLinesetsFromSong(399));
+
---
+++ public/main.js
@@ -0,0 +1,101 @@
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const submit = document.getElementById('submit');
+const songNumber = document.getElementById('songNumber');
+const requestSong = document.getElementById('requestSong');
+const table = document.getElementById("lines");
+
+let currentLine = 0;
+let lineSets = null;
+let highlight = null
+
+submit.addEventListener('click', (e) => {
+   let xHttp = new XMLHttpRequest();
+   xHttp.onreadystatechange = () => {
+     if (xHttp.readyState === 4 && xHttp.status === 200){
+         console.log(xHttp.responseText);
+     }
+   };
+   xHttp.open("GET", `?line1=${line1.value}&line2=${line2.value}`, true);
+   console.log(xHttp);
+   xHttp.send(null);
+});
+
+requestSong.addEventListener('click', (e) => {
+    let xHttp = new XMLHttpRequest();
+    xHttp.onreadystatechange = () => {
+      if (xHttp.readyState === 4 && xHttp.status === 200){
+          console.log(xHttp.responseText);
+          clearList();
+          lineSets = JSON.parse(xHttp.responseText);
+          fillList();
+      }
+    };
+    xHttp.open("GET", `?songNumber=${songNumber.value}`, true);
+    console.log(xHttp);
+    xHttp.send(null);
+ });
+
+ function showlineSetAt(index){
+    let xHttp = new XMLHttpRequest();
+    xHttp.onreadystatechange = () => {
+      if (xHttp.readyState === 4 && xHttp.status === 200){
+          console.log(xHttp.responseText);
+            if(highlight !== null){
+                highlight.classList.remove("highlight");
+            }
+            currentLine = index;
+            highlight = table.rows[currentLine+1];
+            highlight.classList.add("highlight");
+      }
+    };
+    xHttp.open("GET", `?line1=${lineSets[index].line1}&line2=${lineSets[index].line2}`, true);
+    console.log(xHttp);
+    xHttp.send(null);
+ }
+
+ function clearList(){
+     table.innerHTML = "<tr><th>line1</th><th>line2</th></tr>";
+ }
+
+ function fillList(){
+    let id=0
+    lineSets.forEach( (lineSet) => {
+
+        let row = table.insertRow(id + 1);
+        let cell0 = row.insertCell(0);
+        cell0.innerHTML = lineSet.line1;
+        let cell1 = row.insertCell(1);
+        cell1.innerHTML = lineSet.line2;
+        
+        row.addEventListener('click', (e) => {
+            showlineSetAt(e.path[1].rowIndex - 1);
+        });
+        
+        id++;
+    });
+
+    document.onkeydown = (e) => {
+        e = e || window.event;
+        switch (e.key) {
+            case "ArrowLeft":
+                // Left pressed
+                break;
+            case "ArrowRight":
+                // Right pressed
+                break;
+            case "ArrowUp":
+                if(currentLine > 0){
+                    showlineSetAt(currentLine-1);
+                }
+                break;
+            case "ArrowDown":
+                if(currentLine < lineSets.length - 1){
+                    showlineSetAt(currentLine+1);
+                }
+                break;
+        }
+     }
+
+     showlineSetAt(0);
+}
